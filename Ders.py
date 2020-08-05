2472d8be248f1b1cb86055dccd6bc074c0bece12
ramazan <ramazansaribay@gmail.com>
2020-08-05 23:04:03 +0300

    commit all

--- Ders.py
+++ Ders.py
@@ -1,2 +1,6 @@
-x = input("Please write yes or no").title().strip() == "Yes"
-print(x)
+def coord(**x):
+    for key, value in x.items():
+        print(key, value, end="")
+
+coord(N =7463819.958373, E =837463.938473, EL =837.76862,)
+    
