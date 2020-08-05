c741409c5da8a3aa0d405fa95aadc1d4fd64ea35
ramazan <ramazansaribay@gmail.com>
2020-08-05 23:00:12 +0300

    all the small things

--- Ders.py
+++ Ders.py
@@ -1,25 +1,2 @@
-with open("mars.txt", "r", encoding="utf-8") as f:
-    liste = f.readlines()
-counter = 0
-with open("kita.txt", "w", encoding="utf-8") as f:
-    for i in liste:
-        counter += 1
-        if counter %4 == 0:
-            f.write(i + "\n")
-        else:
-            f.write(i)
-
-with open("kita.txt", "r", encoding="utf-8") as f:
-    print(f.read())
-
-
-    
-    
-    
-    
-
-
-
-
-    
-    
+x = input("Please write yes or no").title().strip() == "Yes"
+print(x)
--- Ondülasyon.py
+++ Ondülasyon.py
@@ -1,6 +1,7 @@
 f = open("raw.txt", "r")
 gs = f.readlines()
-x = (gs [9::18])
+x = (gs [9::9])
+x.pop()
 ond = input("What is ondulation?" '\n')
 for line in x:
     line = line.replace("GS,","")
