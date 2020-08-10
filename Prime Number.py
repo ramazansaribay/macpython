77b75978489da5d913ebb9b9aff9b5b92c63d42d
Ramazan SARIBAY <ramazansaribay@gmail.com>
2020-08-10 23:13:41 +0300

    10.08

--- Prime Number.py
+++ Prime Number.py
@@ -1,9 +1,7 @@
-n = int(input('Enter a number to check if it is a prime or not  '))
+asa = int(input('Enter a number to check if it is a prime or not  '))
 count = 0
-for i in range(1, n+1) :
-    if not (n % i) : count += 1
+for i in range(1, asa+1) :
+    if not (asa % i) : count += 1
 
-if (n == 0) or (n == 1) or (count >=3) : print(n, "is not a prime number")
-else: print (n, "is a prime number")
-
-
+if (asa == 0) or (asa == 1) or (count >=3) : print(asa, "is not a prime number")
+else: print (asa, "is a prime number")
